3e8b7e7ad4c271dc95bab1b780250963d8958d85
Pablo89190 <pabloandresgomeze@gmail.com>
2025-12-03 17:00:43 -0500

    Agregar archivo index.html con la estructura básica de la página de inicio y estilos personalizados

--- v1/src/main/java/com/patinaje/v1/controller/indexController.java
+++ v1/src/main/java/com/patinaje/v1/controller/indexController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-
 @RequestMapping("/index")
 public class indexController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index"; 
+    }
+
     @GetMapping("/home")
-    public String Home()
-    {
+    public String Home() {
         return "index";
     }
-    
-
 }
